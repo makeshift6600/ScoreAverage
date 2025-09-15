82b7525e309b6a341fc967ae3df4e74d543b305b
makeshift6600 <j.l.cannady04@gmail.com>
2025-09-15 08:42:04 -0400

    test 4

--- ScoreAverage.cpp
+++ ScoreAverage.cpp
@@ -11,7 +11,7 @@ using namespace std;
 
 int main()
 {
-    int score1, score2, score3, sum;
+    int score1, score2, score3, score4, score5, sum;
     double average;
 
     cout << "please enter your first score " << endl;
@@ -20,7 +20,11 @@ int main()
     cin >> score2;
     cout << "please enter your third score " << endl;
     cin >> score3;
-    average = (score1 + score2 + score3) / 3;
+    cout << "please enter your fourth score " << endl;
+    cin >> score4;
+    cout << "please enter your fifth score " << endl;
+    cin >> score5;
+    average = (score1 + score2 + score3 + score4 + score5) / 3;
     cout << "your average score is " << average;
 
 }
